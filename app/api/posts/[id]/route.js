3bfc666cb462d1830e6843b09a0df090756d2de1
Your Name <you@example.com>
2024-12-11 16:05:15 +0200

    first commit

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -1,27 +1,40 @@
 import { NextResponse } from "next/server";
-import prisma from "../../../lib/prismadb"
+import prisma from "../../../lib/prismadb";
 
 export const GET = async (request, { params }) => {
-  try { 
-    const { id } = params; 
-    const cats = id.split(','); 
+  try {
+    // Ensure `params` is awaited before accessing `id`
+    const { id } = await params; 
+
+    if (!id) {
+      return NextResponse.json(
+        { message: "Invalid request, no ID provided" },
+        { status: 400 }
+      );
+    }
+
+    const cats = id.split(",");
 
     const posts = await prisma.post.findMany({
       where: {
-        id 
-      }
+        id: {
+          in: cats, // Assuming `id` should handle multiple values
+        },
+      },
     });
 
-    if (!posts) {
+    if (!posts || posts.length === 0) {
       return NextResponse.json(
-        { message: "Post not found", err },
+        { message: "Post not found" },
         { status: 404 }
-      )
+      );
     }
 
     return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
-    return NextResponse.json({ message: "GET Error", err }, { status: 500 });
+    return NextResponse.json(
+      { message: "GET Error", error: err.message },
+      { status: 500 }
+    );
   }
 };
-
